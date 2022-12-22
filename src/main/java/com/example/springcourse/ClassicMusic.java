aff8448e05b5a77f3c44e75f7acc52a949ad729d
AVDudarenko <aleksandr.dudareko@gmail.com>
2022-12-22 15:11:45 +0300

    implement IoC

---
+++ src/main/java/com/example/springcourse/ClassicMusic.java
@@ -0,0 +1,8 @@
+package com.example.springcourse;
+
+public class ClassicMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Sonata N5";
+    }
+}
--- src/main/java/com/example/springcourse/TestSpring.java
+++ src/main/java/com/example/springcourse/TestSpring.java
@@ -1,2 +1,13 @@
-package com.example.springcourse;public class TestSpring {
+package com.example.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+        context.close();
+    }
 }
